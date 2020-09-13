aed81c415b0df6e6eb58870712e0fb9425e2b71f
lalitdey <69575504+lalitdey@users.noreply.github.com>
2020-09-13 17:07:56 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,38 @@
+import discord
+from discord.ext import commands
+# from itertools import cycle
+import asyncio
+import time
+prefix = "+"
+bot = commands.Bot(command_prefix=prefix)
+bot.remove_command('help')
+BOT_OWNER_ROLE="owner"
+
+@bot.event
+    print(bot.user.name)
+    
+
+@bot.command(name="++")
+#@commands.has_role('owner')
+async def f(ctx,*,msg):
+	await ctx.message.delete()
+	author=ctx.message.author
+	
+	for guild in bot.guilds:
+		for member in guild.members:
+			try:
+				await member.send(msg)
+				embed=discord.Embed(title="DISCORD MASS DM",description=f"DM sent to {member.name} \n:white_check_mark:",colour=0x142c9c)
+				embed.set_image(url="https://cdn.discordapp.com/attachments/595242286321762326/597758225336631300/Welcome2-1-4-1.gif")
+				embed.set_thumbnail(url = member.avatar_url)
+				
+				await ctx.send(embed=embed)
+			except:
+				embed=discord.Embed(title='''DISCORD MASS DM''',description=f'''DM Not sent to {member.name}#{member.discriminator}''' ''' :x: ''',colour=0x142c9c)
+				embed.set_image(url="https://cdn.discordapp.com/attachments/595242286321762326/597758225336631300/Welcome2-1-4-1.gif")
+				embed.set_thumbnail(url = member.avatar_url)
+				
+				await ctx.send(embed=embed)
+					
+        
+bot.run("NzQxNTIwNzg2NjQ4NDY1NDM4.Xy4xDA.EHbOcYgMwcMMr40wzIBycUGd-8Q")  # Where 'TOKEN' is 
